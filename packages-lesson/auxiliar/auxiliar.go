3605c458653b9c12974c5465e9b2d85c50d4dd41
luc0liv <luoliveiradacunha@gmail.com>
2023-03-28 23:52:06 -0300

    packages practice

---
+++ packages-lesson/auxiliar/auxiliar.go
@@ -0,0 +1,12 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Write: registra uma mensagem na tela
+// A função deve iniciar com letra maiúscula p/ ser referenciada fora do pacote
+func Write() {
+	fmt.Println("Arquivo auxiliar")
+	write2()
+}
---
+++ packages-lesson/auxiliar/auxiliar2.go
@@ -0,0 +1,13 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Write: registra uma mensagem na tela
+func write2() {
+	fmt.Println("Arquivo auxiliar 2")
+}
+
+// funções que começam com letra minúscula
+// só podem ser referenciadas dentro do próprio pacote
